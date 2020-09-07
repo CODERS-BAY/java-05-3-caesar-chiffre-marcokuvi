b1d1fd16f8a73c381ee953e3963c7977236e21f0
Marco <kuvimarco@gmail.com>
2020-09-07 14:25:57 +0200

    Add Ceasar Chiffre Class

--- src/CaesarChiffre.java
+++ src/CaesarChiffre.java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
 public class CaesarChiffre {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Text: ");
         String text = sc.nextLine();
         System.out.println(" Shift: ");
         int shift = sc.nextInt();
         String newtext = "";
-        char alphabet;
-        for(int i=0; i < text.length();i++)
-        {
-            alphabet = text.charAt(i);
 
-            if(alphabet >= 'a' && alphabet <= 'z')
-            {
-                alphabet = (char) + (alphabet + shift);
-                if(alphabet > 'z') {
-                    alphabet = (char) + (alphabet+'a'-'z'-1);
+        for (int i = 0; i < text.length(); i++) {
+            char alphabet = text.charAt(i);
+
+            if (alphabet >= 'a' && alphabet <= 'z') {
+                alphabet = (char) +(alphabet + shift);
+                if (alphabet > 'z') {
+                    alphabet = (char) +(alphabet + 'a' - 'z' - 1);
                 }
                 newtext = newtext + alphabet;
-            }
-            else if(alphabet >= 'A' && alphabet <= 'Z') {
-                alphabet = (char) + (alphabet + shift);
-                if(alphabet > 'Z') {
-                    alphabet = (char) + (alphabet+'A'-'Z'-1);
+            } else if (alphabet >= 'A' && alphabet <= 'Z') {
+                alphabet = (char) +(alphabet + shift);
+                if (alphabet > 'Z') {
+                    alphabet = (char) +(alphabet + 'A' - 'Z' - 1);
                 }
                 newtext = newtext + alphabet;
-            }
-            else {
+            } else {
                 newtext = newtext + alphabet;
             }
 
         }
-        System.out.printf(" Encryptedtext : %s" ,newtext);
+        System.out.printf(" Encryptedtext : %s", newtext);
     }
 }
